faf396f304b19779e501faf0b2e00fc6d2b9e7c0
pravallika <anubothupravallika953@gmail.com>
2025-03-21 22:37:04 +0530

    Adding failure fixes

--- src/test/java/steps/ProductDetailsSteps.java
+++ src/test/java/steps/ProductDetailsSteps.java
@@ -11,67 +11,51 @@ public class ProductDetailsSteps extends Base
 	private ProductDetailsPage productDetailsPage;
 	
 	@Given("User is in the search page")
-	public void user_is_in_the_search_page() throws InterruptedException 
+	public void user_is_in_the_search_page() 
 	{
 		Base.getDriver().get("https://demo.opencart.com.gr/index.php?route=product/search&sort=p.price&order=DESC&search=mac&category_id=18");
-		Thread.sleep(2000);
 		productDetailsPage = new ProductDetailsPage(Base.getDriver());  
 	    System.out.println("User is in the search page to verify the product");
 	}
-	
 	@When("User verifying the product title and clicks")
-	public void user_verifying_the_product_title_and_clicks()
+	public void user_verifying_the_product_title_and_clicks() 
 	{
 	    productDetailsPage.verifyingProductTitle();
 	}
-	
 	@When("User verifying the product description")
-	public void user_verifying_the_product_description() throws InterruptedException 
+	public void user_verifying_the_product_description() 
 	{
-		Thread.sleep(2000);
 		productDetailsPage.verifyingProductDescription();
 	}
-	
 	@When("User verifying the product price")
-	public void user_verifying_the_product_price() throws InterruptedException 
+	public void user_verifying_the_product_price() 
 	{
-		Thread.sleep(2000);
 		productDetailsPage.verifyingProductPrice();
 	}
-	
 	@When("User verifying the product image")
-	public void user_verifying_the_product_image() throws InterruptedException 
+	public void user_verifying_the_product_image() 
 	{
-		Thread.sleep(2000);
 		productDetailsPage.verifyingProductImage();
 	}
-	
 	@When("User verifying the product availability")
-	public void user_verifying_the_product_availability() throws InterruptedException 
+	public void user_verifying_the_product_availability() 
 	{
-		Thread.sleep(2000);
 		productDetailsPage.verifyingProdutctAvailability();
 	}
-	
 	@When("User adding the product to wishlist")
-	public void user_adding_the_product_to_wishlist() throws InterruptedException 
+	public void user_adding_the_product_to_wishlist() 
 	{
-		Thread.sleep(2000);
 		productDetailsPage.addProductToWishlist();
 	}
-	
 	@When("User adding the product to cart")
-	public void user_adding_the_product_to_cart() throws InterruptedException 
+	public void user_adding_the_product_to_cart() 
 	{
-		Thread.sleep(2000);
 		productDetailsPage.addProductToAddToCart();
 	}
-	
 	@Then("User want to verify the cart")
-	public void user_want_to_verify_the_cart() throws InterruptedException
+	public void user_want_to_verify_the_cart()
 	{
-		Thread.sleep(2000);
-		System.out.println("Products has to be verify in the cart");
+	   System.out.println("Prodocts has to be verify in the cart");
 	}
 
 }
--- src/test/java/steps/ShoppingCartSteps.java
+++ src/test/java/steps/ShoppingCartSteps.java
@@ -6,43 +6,43 @@ import io.cucumber.java.en.When;
 import pages.ShoppingCartPage;
 import utility.Base;
 
-public class ShoppingCartSteps extends Base {
-    private ShoppingCartPage shoppingCartPage;
+public class ShoppingCartSteps extends Base
+{
+	private ShoppingCartPage shoppingCartPage;
+	
+	@Given("User is in the shopping cart page")
+	public void user_is_in_the_shopping_cart_page() 
+	{
+		Base.getDriver().get("https://demo.opencart.com.gr/index.php?route=checkout/cart");
+		shoppingCartPage = new ShoppingCartPage(Base.getDriver());  
+	}
+	@When("User clicks on continue shopping and add items to cart")
+	public void user_clicks_on_continue_shopping_and_add_items_to_cart() 
+	{
+		shoppingCartPage.inShoppingCartPage();
+		shoppingCartPage.addingItemToCart();
+	}
+	@When("User clcik on the shopping cart page")
+	public void user_clcik_on_the_shopping_cart_page() 
+	{
+		shoppingCartPage.redirectingToShoppingCartPage();
+	}
+	@When("User removing the item added")
+	public void user_removing_the_item_added() throws InterruptedException 
+	{
+		Thread.sleep(3000);
+		shoppingCartPage.removingItemInCart();
+	}
+	@When("User updating the items quantity in the cart")
+	public void user_updating_the_items_quantity_in_the_cart() 
+	{
+		shoppingCartPage.updatingItemInCart();
+	}
+	@Then("User clicks on checkout")
+	public void user_clicks_on_checkout()
+	{
+		shoppingCartPage.checkout();
+	}
 
-    @Given("User is on the shopping cart page")
-    public void user_is_on_the_shopping_cart_page() {
-        Base.getDriver().get("https://demo.opencart.com.gr/index.php?route=checkout/cart");
-        shoppingCartPage = new ShoppingCartPage(Base.getDriver());
-    }
 
-    @When("User clicks on continue shopping and adds items to cart")
-    public void user_clicks_on_continue_shopping_and_adds_items_to_cart() throws InterruptedException {
-        Thread.sleep(2000);
-        shoppingCartPage.inShoppingCartPage();
-        shoppingCartPage.addingItemToCart();
-    }
-
-    @When("User clicks on the shopping cart page")
-    public void user_clicks_on_the_shopping_cart_page() throws InterruptedException {
-        Thread.sleep(2000);
-        shoppingCartPage.redirectingToShoppingCartPage();
-    }
-
-    @When("User removes the added item")
-    public void user_removes_the_added_item() throws InterruptedException {
-        Thread.sleep(2000);
-        shoppingCartPage.removingItemInCart();
-    }
-
-    @When("User updates the item quantity in the cart")
-    public void user_updates_the_item_quantity_in_the_cart() throws InterruptedException {
-        Thread.sleep(2000);
-        shoppingCartPage.updatingItemInCart();
-    }
-
-    @Then("User proceeds to checkout")
-    public void user_proceeds_to_checkout() throws InterruptedException {
-        Thread.sleep(2000);
-        shoppingCartPage.checkout();
-    }
 }
